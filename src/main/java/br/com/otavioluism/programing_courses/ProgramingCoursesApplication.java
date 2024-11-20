9edbfdca7d7373ad02edcb7e3909ff13d8eefa36
otavioluism <otavioluism97@hotmail.com>
2024-11-20 11:32:34 -0300

    iniciando projeto

---
+++ src/main/java/br/com/otavioluism/programing_courses/ProgramingCoursesApplication.java
@@ -0,0 +1,13 @@
+package br.com.otavioluism.programing_courses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgramingCoursesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgramingCoursesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/otavioluism/programing_courses/ProgramingCoursesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.otavioluism.programing_courses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgramingCoursesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
